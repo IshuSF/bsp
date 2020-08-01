fbf435e7710442573d1e215cd9c8b9a801a90bb2
ishu-sf <ishu.singhal@sourcefuse.com>
2020-08-01 10:44:41 +0530

    initial project

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {NgxPrintModule} from 'ngx-print';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxPrintModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
